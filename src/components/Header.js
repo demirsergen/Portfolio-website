2b9893484e8cb241db4ef29580dfbd219ebd5b54
Sergen Demir <dmrsrgn@gmail.com>
2021-10-16 17:07:26 +0300

    add scrollUp function to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
+import { useEffect } from "react";
 import { Link, Switch } from "react-router-dom";
 import { GrCompass } from "react-icons/gr";
 import "../styles.css";
 
 const Header = () => {
 
+  const scrollUp = () => {
+    window.scrollTo(0, 0);
+  }
+
   return (
     <Switch>
       <header>
       <div className="headerContainer">
-        <div className="brand">
+        <div className="brand" onClick={scrollUp}>
           <Link to="/">
           <GrCompass size="40px" />
           <h2>Sergen Demir</h2>
@@ -16,16 +21,16 @@ const Header = () => {
         </div>
         <ul className="navLinks">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={scrollUp}>Home</Link>
           </li>
           <li>
-            <Link to="/Projects">Projects</Link>
+            <Link to="/Projects" onClick={scrollUp}>Projects</Link>
           </li>
           <li>
-            <Link to="/About">About</Link>
+            <Link to="/About" onClick={scrollUp}>About</Link>
           </li>
           <li>
-            <Link to="/Contact">Contact</Link>
+            <Link to="/Contact" onClick={scrollUp}>Contact</Link>
           </li>
         </ul>
       </div>
