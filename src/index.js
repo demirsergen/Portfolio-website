e2f942e5c042b587ea2cf10321afd7ff8978c961
sergen demir <dmrsrgn@gmail.com>
2022-04-04 12:23:10 -0500

    update to react@18

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-    <App />,
-  rootElement
-);
+const root = createRoot(rootElement);
+root.render(<App />);
