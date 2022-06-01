26af2b39684fd30a574aaab77a56f8e092dc1f43
sergen demir <dmrsrgn@gmail.com>
2022-05-31 20:11:42 -0500

    update projects

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,44 +1,44 @@
-import image0 from '../static/images/wordle-clone.png';
-import image1 from '../static/images/note-taking-app.png';
-import image2 from '../static/images/prs.png';
-import image3 from '../static/images/weatherapp.png';
-import image4 from '../static/images/personal-website.png';
+import image0 from "../static/images/wordle-clone.png";
+import image1 from "../static/images/note-taking-app.png";
+import image2 from "../static/images/e-commerce.png";
+import image3 from "../static/images/weatherapp.png";
+import image4 from "../static/images/personal-website.png";
 
- 
 export const data = [
   {
     name: "Personal Website",
     tags: ["React", "CSS"],
     img: image4,
     visit: "https://sergendemir.com",
-    source: "https://github.com/demirsergen/Portfolio-website"
+    source: "https://github.com/demirsergen/Portfolio-website",
   },
   {
     name: "Wordle clone",
     tags: ["React", "CSS"],
     img: image0,
     visit: "https://wordle-clone2022.netlify.app",
-    source: "https://github.com/demirsergen/wordle-clone"
+    source: "https://github.com/demirsergen/wordle-clone",
+  },
+  {
+    name: "E-commerce",
+    tags: ["React", "CSS", "Firebase"],
+    img: image2,
+    visit: "https://e-commerce-react-practice.netlify.app/",
+    source: "https://github.com/demirsergen/e-commerce",
   },
   {
     name: "Note Taking App",
     tags: ["React", "CSS"],
     img: image1,
     visit: "https://note-taking-app-2021.netlify.app/",
-    source: "https://github.com/demirsergen/note-taking-app"
+    source: "https://github.com/demirsergen/note-taking-app",
   },
-  {
-    name: "Rock-Paper-Scissors",
-    tags: ["JS", "CSS"],
-    img: image2,
-    visit: "https://rock-paper-sci.netlify.app",
-    source: "https://github.com/demirsergen/rock-paper-scissors-master"
-  },
+
   {
     name: "Weather App",
     tags: ["JS", "CSS"],
-    img:image3,
+    img: image3,
     visit: "https://weather2022app.netlify.app",
-    source: "https://github.com/demirsergen/weather-app2"
-  }
+    source: "https://github.com/demirsergen/weather-app2",
+  },
 ];
