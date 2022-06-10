3cdb6903490dcc688aab9a2b5e0c83af44a13b45
sergen demir <dmrsrgn@gmail.com>
2022-06-10 12:12:59 -0500

    delete token

--- src/client.js
+++ src/client.js
@@ -4,6 +4,6 @@ export default sanityClient({
   projectId: "kma5le70",
   dataset: "production",
   apiVersion: "2021-08-31",
-  token: process.env.REACT_APP_TOKEN,
+  token: "",
   useCdn: true,
 });
