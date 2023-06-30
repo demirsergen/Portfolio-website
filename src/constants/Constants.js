52350044fe6dc7bbb61517b02c9459eb7caf6737
Sergen Demir <dmrsrgn@gmail.com>
2023-06-30 16:52:34 -0400

    update project name

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,52 +1,52 @@
-import image0 from "../static/images/wordle-clone.png";
-import image1 from "../static/images/note-taking-app.png";
-import image2 from "../static/images/e-commerce.png";
-import image3 from "../static/images/weatherapp.png";
-import image4 from "../static/images/personal-website.png";
-import image5 from "../static/images/braindumps.png";
+import image0 from '../static/images/wordle-clone.png';
+import image1 from '../static/images/note-taking-app.png';
+import image2 from '../static/images/e-commerce.png';
+import image3 from '../static/images/weatherapp.png';
+import image4 from '../static/images/personal-website.png';
+import image5 from '../static/images/braindumps.png';
 
 export const data = [
   {
-    name: "Personal Website",
-    tags: ["React", "CSS"],
+    name: 'Personal Website',
+    tags: ['React', 'CSS'],
     img: image4,
-    visit: "https://sergendemir.com",
-    source: "https://github.com/demirsergen/Portfolio-website",
+    visit: 'https://sergendemir.com',
+    source: 'https://github.com/demirsergen/Portfolio-website',
   },
   {
-    name: "Braindumps - Idea Sharing",
-    tags: ["Next JS", "Tailwind", "Firebase"],
+    name: 'Braindump - Social Media',
+    tags: ['Next JS', 'Tailwind', 'Firebase'],
     img: image5,
-    visit: "https://braindumps.netlify.app",
-    source: "https://github.com/demirsergen/brain-dump",
+    visit: 'https://braindumps.netlify.app',
+    source: 'https://github.com/demirsergen/brain-dump',
   },
   {
-    name: "Wordle Clone",
-    tags: ["React", "CSS"],
+    name: 'Wordle Clone',
+    tags: ['React', 'CSS'],
     img: image0,
-    visit: "https://wordle-clone2022.netlify.app",
-    source: "https://github.com/demirsergen/wordle-clone",
+    visit: 'https://wordle-clone2022.netlify.app',
+    source: 'https://github.com/demirsergen/wordle-clone',
   },
   {
-    name: "E-commerce",
-    tags: ["React", "CSS", "Firebase"],
+    name: 'E-commerce',
+    tags: ['React', 'CSS', 'Firebase'],
     img: image2,
-    visit: "https://e-commerce-react-practice.netlify.app/",
-    source: "https://github.com/demirsergen/e-commerce",
+    visit: 'https://e-commerce-react-practice.netlify.app/',
+    source: 'https://github.com/demirsergen/e-commerce',
   },
   {
-    name: "Note Taking App",
-    tags: ["React", "CSS"],
+    name: 'Note Taking App',
+    tags: ['React', 'CSS'],
     img: image1,
-    visit: "https://note-taking-app-2021.netlify.app/",
-    source: "https://github.com/demirsergen/note-taking-app",
+    visit: 'https://note-taking-app-2021.netlify.app/',
+    source: 'https://github.com/demirsergen/note-taking-app',
   },
 
   {
-    name: "Weather App",
-    tags: ["JS", "CSS"],
+    name: 'Weather App',
+    tags: ['JS', 'CSS'],
     img: image3,
-    visit: "https://weather2022app.netlify.app",
-    source: "https://github.com/demirsergen/weather-app2",
+    visit: 'https://weather2022app.netlify.app',
+    source: 'https://github.com/demirsergen/weather-app2',
   },
 ];
