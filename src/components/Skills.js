2d402a96becff393a59b8168634e35940a31351c
Sergen Demir <dmrsrgn@gmail.com>
2023-07-04 10:08:20 -0400

    add new skill icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,17 +1,19 @@
 import {
   SiJavascript,
   SiReact,
+  SiPython,
+  SiMysql,
   SiTypescript,
   SiStyledComponents,
   SiTailwindcss,
   SiFirebase,
   SiRedux,
   SiNextDotJs,
-} from "react-icons/si";
+} from 'react-icons/si';
 
-import { FaSass, FaNode } from "react-icons/fa";
+import { FaSass, FaNode } from 'react-icons/fa';
 
-import "../styles.css";
+import '../styles.css';
 
 const Skills = () => {
   return (
@@ -27,8 +29,20 @@ const Skills = () => {
           <p>React JS</p>
         </div>
         <div className="stackIconsContainer__skill">
+          <SiPython className="skillIcon" />
+          <p>Python</p>
+        </div>
+        <div className="stackIconsContainer__skill">
           <SiNextDotJs className="skillIcon" />
           <p>NextJS</p>
+        </div>
+        <div className="stackIconsContainer__skill">
+          <SiTypescript className="skillIcon" />
+          <p>Typescript</p>
+        </div>
+        <div className="stackIconsContainer__skill">
+          <SiMysql className="skillIcon" />
+          <p>MySQL</p>
         </div>
         <div className="stackIconsContainer__skill">
           <FaSass className="skillIcon" />
@@ -37,6 +51,10 @@ const Skills = () => {
         <div className="stackIconsContainer__skill">
           <SiTailwindcss className="skillIcon" />
           <p>Tailwind CSS</p>
+        </div>
+        <div className="stackIconsContainer__skill">
+          <SiStyledComponents className="skillIcon" />
+          <p>Styled Components</p>
         </div>
         <div className="stackIconsContainer__skill">
           <SiFirebase className="skillIcon" />
