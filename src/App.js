525fa6274b07fd4ac91fb450911e67c6d7d97e6b
Sergen Demir <dmrsrgn@gmail.com>
2024-04-06 12:11:55 -0500

    update resume

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./styles.css";
-import Projects from "./components/Projects";
-import Home from "./components/Home";
-import Header from "./components/Header";
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Contact from "./components/Contact";
-import Blog from "./components/Blog";
-import BlogPost from "./components/BlogPost";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './styles.css';
+import Projects from './components/Projects';
+import Home from './components/Home';
+import Header from './components/Header';
+import About from './components/About';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Blog from './components/Blog';
+import BlogPost from './components/BlogPost';
 
 export default function App() {
   return (
